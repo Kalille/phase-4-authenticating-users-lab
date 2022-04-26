aa94b09556ed3c827c956296c70d28759b51d440
kalille Overton <>
2022-04-26 12:26:44 -0400

    commit tree 677e7aa73e666d1b3d9a59ffc2ee15d5d2c983e3

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,11 @@
+require 'pry'
+
 class SessionsController < ApplicationController
 
     def create
-        user = User.find_by(params[:user_name])
+      
+        user = User.find_by(username: params[:username])
+        # binding.pry
         session[:user_id] = user.id 
         render json: user
     end
