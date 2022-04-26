22c187373ca24b417b6d258d0049017c82dc6e47
kalille Overton <>
2022-04-26 12:07:09 -0400

    updated

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,10 @@
+class UsersController < ApplicationController
+def index
+render json: User.all
+end
+    def show
+        user = User.find(session[:user_id])
+    
+       render json: user
+    end
+end
